c73edd1377daa846db1ae536998fdf6ab690757b
Maria K <klimenteva.mr@phystech.edu>
2023-04-16 13:29:10 +0300

    #

--- graphics_sdl/include/map.h
+++ graphics_sdl/include/map.h
@@ -6,18 +6,20 @@
 
 #include "tile.h"
 #include "define.h"
-
+//--------------------------------------------------------------------------
 class Map
 {
     public:
     Map();
+
     SDL_Surface* Surf_Tileset;
-    bool OnLoad(char* File); //загружает карту из файла
+
+    bool OnLoad(char* File); //загружает карту из файла и заполняет список плиток
     void OnRender(SDL_Surface* Surf_Display, int MapX, int MapY); //рисут карту и размещает все плитки на их места
     //MapX, MapY указывают, где отображать карту на экране
 
     private:
-    std::vector<Tile> TileList;//хранит список всех плиток карты
+    std::vector<Tile> TileList; //хранит список всех плиток карты
 };
-
+//--------------------------------------------------------------------------
 #endif // MAP_H
